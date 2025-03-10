98447b3640b33e3c87234e146295f4c22ebd5653
Bekian <Bekian.Cat@gmail.com>
2025-03-09 20:43:07 -0500

    feat(app): added query handler
    added a simple implementation to write the result of the query into a simple html string and return that as a response, also see changes

--- build.zig
+++ build.zig
@@ -4,39 +4,30 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    // import deps
     const sqlite = b.dependency("sqlite", .{
         .target = target,
         .optimize = optimize,
     });
-
-    exe_mod.addImport("sqlite", sqlite.module("sqlite"));
-
-    exe_mod.addImport("zipi_lib", lib_mod);
-
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "zipi",
-        .root_module = lib_mod,
+    const httpz = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
     });
-
-    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "zipi",
         .root_module = exe_mod,
     });
+
+    // apply imports
+    exe.root_module.addImport("sqlite", sqlite.module("sqlite"));
+    exe.root_module.addImport("httpz", httpz.module("httpz"));
 
     b.installArtifact(exe);
 
@@ -50,13 +41,6 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
     });
@@ -64,6 +48,5 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,60 @@
 const sqlite = @import("sqlite");
 const std = @import("std");
+const httpz = @import("httpz");
+// first we base the app on the httpz example 2
+// global consts are CAPS
+const PORT = 8080;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) {
-        std.debug.print("mem leak UwU", .{});
+        std.debug.print("mem leak oopsies", .{});
     };
     const allocator = gpa.allocator();
 
-    var server = std.http.Server.init(allocator, .{});
+    var handler = Handler{};
+    var server = try httpz.Server(*Handler).init(allocator, .{ .port = PORT }, &handler);
     defer server.deinit();
+    defer server.stop();
 
-    const addr = try std.net.Address.parseIp("127.0.0.1", 8080);
-    try server.listen(addr);
+    var router = try server.router(.{});
+    router.get("/", index, .{});
+    router.get("/sendmsg", sendMsg, .{});
 
-    std.debug.print("Server listening on http://127.0.0.1:8080\n", .{});
+    std.debug.print("listening on http://localhost:{d}/\n", .{PORT});
+
+    try server.listen();
+}
+
+const Handler = struct {
+    _hits: usize = 0,
+
+    // this is for a 400 error
+    pub fn not_found(_: *Handler, _: *httpz.Request, res: *httpz.Response) !void {
+        res.status = 404;
+        res.body = "Not found";
+    }
+
+    // this is for a 500 error
+    pub fn uncaughtError(_: *Handler, req: *httpz.Request, res: *httpz.Response, err: anyerror) void {
+        std.debug.print("uncaught http error at {s}: {}\n", .{ req.url.path, err });
+
+        res.headers.add("content-type", "text/html; charset=utf-8");
+        res.status = 505;
+        res.body = "<!DOCTYPE html> 505 error";
+    }
+};
+
+fn index(_: *Handler, _: *httpz.Request, res: *httpz.Response) !void {
+    res.body =
+        \\<!DOCTYPE html>
+        \\<h1>Hello World!</h1>
+    ;
+}
+
+fn sendMsg(_: *Handler, req: *httpz.Request, res: *httpz.Response) !void {
+    const output = try std.fmt.allocPrint(res.arena, "<!DOCTYPE html>\n<html><body><h1>Hello {s}!</h1></body></html>", .{req.url.query});
+    res.content_type = httpz.ContentType.HTML;
+    res.status = 200;
+    res.body = output;
 }
